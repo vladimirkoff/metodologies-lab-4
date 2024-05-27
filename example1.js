74d80232d62581e311e1ccb04deec85a03c422bf
Vladimir Kovalev <vovakovalev2016@icloud.com>
2024-05-27 19:25:21 +0300

    update dictionary, remove unnecessary files

--- example1.js
+++
@@ -1,4 +0,0 @@
-// Start of the program
-"use strict";
-console.log("HAI WORLD!");
-// End of the program
--- js/dictionary.js
+++ js/dictionary.js
@@ -2,5 +2,9 @@ module.exports = {
   'HAI': `// Start of the program\n'use strict';\n`,
   'CAN HAS': '',
   'VISIBLE': (line) => line.split(' ')[1] === 'FILE' ? `console.log(data) ` : line.split('"')[1] === undefined ? `console.log(${line.split(' ')[1]});\n` : `console.log('${line.split('"')[1]}');\n`,
-  'KTHXBYE': '// End of the program\n'
+  'KTHXBYE': '// End of the program\n',
+  'PLZ OPEN FILE': (line) => `const fs = require('fs');\nfs.readFile('${line.split('"')[1]}', 'utf8', (err, data) => `,
+  'AWSUM THX': '!err ? ',
+  'O NOES': ': ',
+  'INVISIBLE': (line) => `console.error('${line.split('"')[1]}'));\n`
 }
